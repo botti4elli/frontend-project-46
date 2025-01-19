6b162761b348af4da1820e8bfaae9b65f962f824
Daria Semenkina <apple-003@mail.ru>
2025-01-19 20:56:07 +0400

    tests

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,27 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { expect, test } from '@jest/globals';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('genDiff compares flat JSON files and returns the correct diff', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  const expectedDiff = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+  const result = genDiff(filepath1, filepath2);
+
+  expect(result).toBe(expectedDiff);
+});
