3e0102c8d8a43c4e71fc4c456ae685f1f433f0d0
Daria Semenkina <apple-003@mail.ru>
2025-01-28 16:11:58 +0400

    step 9

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,20 +7,20 @@ import genDiff from '../src/genDiff.js';
 const program = new Command();
 
 program
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format [type]', 'output format', 'default')
-    .action((filepath1, filepath2, options) => {
-        try {
-            const absolutePath1 = path.resolve(process.cwd(), filepath1);
-            const absolutePath2 = path.resolve(process.cwd(), filepath2);
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'default')
+  .action((filepath1, filepath2, options) => {
+    try {
+      const absolutePath1 = path.resolve(process.cwd(), filepath1);
+      const absolutePath2 = path.resolve(process.cwd(), filepath2);
 
-            const diff = genDiff(absolutePath1, absolutePath2, options.format);
-            console.log(diff);
-        } catch (error) {
-            console.error(`Error: ${error.message}`);
-        }
-    });
+      const diff = genDiff(absolutePath1, absolutePath2, options.format);
+      console.log(diff);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+    }
+  });
 
 program.parse(process.argv);
