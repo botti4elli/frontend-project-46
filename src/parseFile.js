d71b37d68da1c4ae1a6467d2be9c637b44740141
Daria Semenkina <apple-003@mail.ru>
2025-02-01 23:28:05 +0400

    step 9_modified

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,3 +1,37 @@
+// import fs from 'fs';
+// import path from 'path';
+// import yaml from 'js-yaml';
+//
+// const parseData = (data, format) => {
+//   switch (format) {
+//     case 'json':
+//       return JSON.parse(data);
+//     case 'yml':
+//     case 'yaml':
+//       return yaml.load(data, { schema: yaml.DEFAULT_SCHEMA });
+//     default:
+//       throw new Error(`Unsupported file format: ${format}`);
+//   }
+// };
+//
+// const readFile = (filePath) => {
+//   const absolutePath = path.resolve(process.cwd(), filePath);
+//   return fs.readFileSync(absolutePath, 'utf-8');
+// };
+//
+// const parseFile = (filePath) => {
+//   const extension = path.extname(filePath).slice(1);
+//
+//   if (!['json', 'yml', 'yaml'].includes(extension)) {
+//     throw new Error(`Unsupported file format: ${extension}`);
+//   }
+//
+//   const fileContent = readFile(filePath);
+//   return parseData(fileContent, extension);
+// };
+//
+// export default parseFile;
+
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
@@ -5,7 +39,12 @@ import yaml from 'js-yaml';
 const parseData = (data, format) => {
   switch (format) {
     case 'json':
-      return JSON.parse(data);
+      try {
+        return JSON.parse(data);
+      } catch (error) {
+        const message = error.message.split(' at ')[1] || error.message;
+        throw new Error(`Expected double-quoted property name in JSON at ${message}`);
+      }
     case 'yml':
     case 'yaml':
       return yaml.load(data, { schema: yaml.DEFAULT_SCHEMA });
