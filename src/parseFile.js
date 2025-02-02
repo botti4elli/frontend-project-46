cf0cdbbd6715fb5a2e6804b7962661a62dbb3241
Daria Semenkina <apple-003@mail.ru>
2025-02-02 22:13:19 +0400

    9

--- src/parseFile.js
+++ src/parseFile.js
@@ -18,34 +18,6 @@ const parseData = (data, format) => {
   throw new Error(`Unsupported file format: ${format}`);
 };
 
-// const parseData = (data, format) => {
-//   if (format === 'json') {
-//     try {
-//       return JSON.parse(data);
-//     } catch (error) {
-//       const { message } = error;
-//       const positionIndex = message.indexOf('position ');
-//
-//       if (positionIndex !== -1) {
-//         const position = Number(message.slice(positionIndex + 9).split(' ')[0]);
-//         const beforeError = data.slice(0, position);
-//         const line = beforeError.split('\n').length;
-//         const column = position - beforeError.lastIndexOf('\n');
-//
-//         throw new Error(`Expected double-quoted property name in JSON at position ${position} (line ${line} column ${column})`);
-//       }
-//
-//       throw new Error(message);
-//     }
-//   }
-//
-//   if (format === 'yml' || format === 'yaml') {
-//     return yaml.load(data, { schema: yaml.DEFAULT_SCHEMA });
-//   }
-//
-//   throw new Error(`Unsupported file format: ${format}`);
-// };
-
 const readFile = (filePath) => {
   const absolutePath = path.resolve(process.cwd(), filePath);
   return fs.readFileSync(absolutePath, 'utf-8');
