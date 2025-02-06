06d0e5ef3d7432e1e13518d88bdff58e1b8dcbc7
botti4elli <apple-003@mail.ru>
2025-02-06 06:19:37 -0800

    refactor

--- src/formatters/index.js
+++
@@ -1,9 +0,0 @@
-// import formatStylish from './stylish.js';
-// import formatPlain from './plain.js';
-//
-// const formatters = {
-//   stylish: formatStylish,
-//   plain: formatPlain,
-// };
-//
-// export default formatters;
